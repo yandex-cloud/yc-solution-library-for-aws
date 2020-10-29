ae441ffa9ab2e18a744956e5f10f2af6a68ac948
Narek Tatevosyan <nrkk@yandex-team.ru>
2020-10-29 23:30:01 +0300

    added terraform functions

--- s3 Sync/example/01-aws-lamdba.tf
+++ s3 Sync/example/01-aws-lamdba.tf
@@ -13,7 +13,9 @@ resource "aws_lambda_function" "yandex_sync" {
 
   environment {
     variables = {
-      AWS_BUCKET = aws_s3_bucket.aws_yc_sync.id 
+      AMAZON_BUCKET = aws_s3_bucket.aws_yc_sync.id
+      AMAZON_ACCESS_KEY_ID = aws_iam_access_key.yandex_sync.id
+      AMAZON_SECRET_ACCESS_KEY = aws_iam_access_key.yandex_sync.secret 
       YANDEX_BUCKET = yandex_storage_bucket.aws_yc_sync.id
       YANDEX_ACCESS_KEY_ID = yandex_iam_service_account_static_access_key.sa_static_key.access_key
       YANDEX_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.sa_static_key.secret_key
